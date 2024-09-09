8a590dda49772e8e2a71b9010257b8a9fb5cb57e
Mark Narvidas <work@MaBookAir.local>
2024-09-09 20:46:21 +0100

    add tabs, BPM button placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Coffee } from "@/components/coffee/Coffee";
 import { InlinePicker } from "@/components/picker/Picker";
 import { usePersistedState } from "@/hooks/usePersistedState";
@@ -7,42 +8,67 @@ import { usePersistedState } from "@/hooks/usePersistedState";
 export default function Home() {
   const [bpm1, setBpm1] = usePersistedState("beatmatchin-bpm1", 128);
   const [bpm2, setBpm2] = usePersistedState("beatmatchin-bpm2", 128);
+  const [activeTab, setActiveTab] = useState("pitchAdj");
 
   return (
     <div className="flex flex-col items-center justify-items-center h-screen overflow-hidden p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start flex-grow">
-        <div className="list-inside list-decimal text-sm flex w-full flex-col align-center text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <h1 className="text-3xl font-bold mb-8 text-center">
-            Beatmatch Calculator
-          </h1>
-          <div className="mb-2 text-center">
-            Calculate PITCH ADJ value based on track BPMs{" "}
-          </div>
-        </div>
-        <div className="flex justify-center items-center w-full mt-8">
-          <div className="flex flex-row gap-20">
-            <div>
-              <div className="text-center">Track 1</div>
-              <div className="h-40 overflow-y-scroll no-scrollbar">
-                <InlinePicker onNewBpm={setBpm1} defaultBpm={bpm1} />
-              </div>
+        <div className="flex justify-center items-center w-80">
+          <div className="flex flex-col gap-4 w-full">
+            <div className="flex border-b border-gray-200 text-sm mb-8">
+              <button
+                className={`py-2 px-4 w-40 ${activeTab === "pitchAdj" ? "border-b-2 border-white-500" : "opacity-50"}`}
+                onClick={() => setActiveTab("pitchAdj")}
+              >
+                Calculator
+              </button>
+              <button
+                className={`py-2 px-4 w-40 ${activeTab === "empty" ? "border-b-2 border-slate-500" : "opacity-50"}`}
+                onClick={() => setActiveTab("empty")}
+              >
+                Find BPM
+              </button>
             </div>
-            <div>
-              <div className="text-center">Track 2</div>
-              <div className="h-40 overflow-y-scroll no-scrollbar">
-                <InlinePicker onNewBpm={setBpm2} defaultBpm={bpm2} />
+            {activeTab === "pitchAdj" && (
+              <>
+                <div className="list-inside list-decimal text-sm flex w-full flex-col align-center text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+                  <h1 className="text-3xl font-bold mb-8 text-center">Beatmatch Calculator</h1>
+                  <div className="mb-2 text-center">Calculate PITCH ADJ value based on track BPMs </div>
+                </div>
+
+                <div className="flex justify-center items-center w-full mt-8">
+                  <div className="flex flex-row gap-20">
+                    <div>
+                      <div className="text-center">Track 1</div>
+                      <div className="h-40 overflow-y-scroll no-scrollbar">
+                        <InlinePicker onNewBpm={setBpm1} defaultBpm={bpm1} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-center">Track 2</div>
+                      <div className="h-40 overflow-y-scroll no-scrollbar">
+                        <InlinePicker onNewBpm={setBpm2} defaultBpm={bpm2} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center -mt-8">
+                  <div className="text-center mb-2">Pitch Adjustment Value</div>
+                  <div className="text-4xl font-bold text-center">
+                    {Math.round((100 * (bpm1 - bpm2)) / (bpm2 * 0.01)) / 100}
+                  </div>
+                </div>
+              </>
+            )}
+            {activeTab === "empty" && (
+              <div className="text-center flex flex-col items-center">
+                <p className="mb-12">Tap the button to find BPM</p>
+                <div className="text-2xl font-bold mb-12">-- BPM</div>
+                <button className="w-24 h-24 rounded-full bg-white text-black font-bold shadow-lg hover:shadow-xl transition-shadow">
+                  TAP
+                </button>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-center items-center w-full">
-          <div className="flex flex-col gap-4">
-            <div className="text-center">
-              <div className="text-center mb-2">Pitch Adjustment Value</div>
-              <div className="text-4xl font-bold text-center">
-                {Math.round((100 * (bpm1 - bpm2)) / (bpm2 * 0.01)) / 100}
-              </div>
-            </div>
+            )}
           </div>
         </div>
       </main>
